e742dd469f3f348b99fa63e36a94320360abdce7
Yang Honggang <rtlinux@163.com>
2020-05-24 19:45:00 +0800

    rbd_lat_parse.py: rbd test latency parser
    6626965333486812956 [0, 24, 69, 391, 482, 498] <msg, iostack, other>: 136 322 40
    4829746370210484920 [0, 40, 82, 401, 490, 503] <msg, iostack, other>: 131 319 53
    4276924629996746318 [0, 23, 81, 419, 511, 525] <msg, iostack, other>: 150 338 37
    2824576997871255188 [0, 26, 72, 391, 478, 491] <msg, iostack, other>: 133 319 39
    
    Signed-off-by: Yang Honggang <rtlinux@163.com>

---
+++ rbd_lat_parse.py
@@ -0,0 +1,88 @@
+import babeltrace
+import sys
+
+debug = False
+trace_path = '../data/single-node-3-reps'
+
+def debug_print(*args):
+    if debug:
+        print('>>>', args)
+
+def reap(times, trace_id):
+    '''
+    calc timecost of each stage
+    '''
+    times = [int(v) for k,v in times.items()]
+    times = [i - times[0] for i in times]
+    if len(times) == 6:
+        other = 0
+        iostack = 0
+        msg = 0
+
+        other += times[1] - times[0]
+        msg += times[2] - times[1]
+        iostack += times[3] - times[2]
+        msg += times[4] - times[3]
+        other += times[5] - times[4]
+
+        print(trace_id, times, "<msg, iostack, other>:", msg, iostack, other)
+    else:
+        debug_print(trace_id, "ERROR", times)
+
+def fire():
+    trace_collection = babeltrace.TraceCollection()
+    trace_collection.add_traces_recursive(trace_path, 'ctf')
+    
+    collector = {}
+    for event in trace_collection.events:
+        #Extract event information
+        name = event.name
+        span_id = int(event["span_id"])
+        trace_id = int(event["trace_id"])
+        parent_span_id = int(event["parent_span_id"])
+        port = event["port_no"]
+        trace_name = event["trace_name"]
+        service_name = event["service_name"]
+        ip = event["ip"]
+        #Use CS as default value for Zipkin annotations
+        value = "cs"
+        priority = 0
+        if "core_annotation" in event:
+            value =  event["core_annotation"]
+        if "event" in event:
+            event_name = event["event"]
+        timestamp = str(event.timestamp)[:-3]
+    
+        if 'val' in event:
+            priority = event['val']
+        e = (span_id, parent_span_id, trace_id, trace_name, service_name, ip, 
+               port, timestamp, event_name, priority)
+        debug_print(e)
+        if parent_span_id == 0 and event_name == 'init':
+            collector[trace_id] = {'1' : timestamp}
+        else:
+            if trace_id in collector:
+                if (trace_name == 'op msg' and 
+                    service_name == 'Objecter' and
+                    event_name == 'async enqueueing message'):
+                    collector[trace_id]['2'] = timestamp
+                elif trace_name == 'osd op' and event_name == 'message destructed':
+                    debug_print(trace_id, e) 
+                    # primary osd op(priority = 63)
+                    if priority == 63:
+                        collector[trace_id]['3'] = timestamp
+                elif event_name == 'sup_op_commit':
+                    collector[trace_id]['4'] = timestamp
+                elif event_name == 'osd op reply':
+                    collector[trace_id]['5'] = timestamp
+                elif (trace_name.startswith('write rbd_data') and
+                     event_name == 'finish'):
+                   collector[trace_id]['6'] = timestamp
+           
+                   # reap
+                   debug_print(trace_id, collector[trace_id])
+                   reap(collector[trace_id], trace_id)
+                   collector.pop(trace_id)
+
+if __name__ == '__main__':
+    fire()
